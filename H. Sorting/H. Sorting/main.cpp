61e5cc9facc70f8de42e19f5086d075c98971763
abadin-Fahim <joynalabadinfahim@gmail.com>
2025-10-26 14:45:32 +0100

    solved

---
+++ H. Sorting/H. Sorting/main.cpp
@@ -0,0 +1,27 @@
+//
+//  main.cpp
+//  H. Sorting
+//
+//  Created by Joynal Abadin on 26.10.25.
+//
+
+#include <iostream>
+//#include <algorithm>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    sort(arr, arr + n);
+    
+    for (int i = 0; i < n; i++) {
+        
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
